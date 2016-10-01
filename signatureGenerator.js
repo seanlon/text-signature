7540f780ecb12a9b76e22850f3424ed11c3359ee
sean lon <seanlon.loh@bertelsmann.de>
2016-10-01 17:16:55 +0800

    add custom font support

--- signatureGenerator.js
+++ signatureGenerator.js
@@ -4,15 +4,23 @@ $('#nameForm').on('submit', function(e) {
     var fName = $('[name="fname"] ').val();
     var lName = $('[name="lname"] ').val();
 
-    var textString = fName +" " +  lName;
-    var fontStyleList = ["50px", "OpenSanss" , "bold"]; 
+    var textString = fName + " " + lName;
+
+    var fontStyleList = ["50px", "'Homemade Apple'"];
     var customFont = {
-        name:"OpenSanss", "url" :"http://fonts.google.com/specimen/Pacifico?selection.family=Oswald|Pacifico"
+        name: "'Homemade Apple'",
+        "url": "http://fonts.googleapis.com/css?family=Homemade+Apple"
+
     }
     var paramSignatureText = {
-        width:300,height:300, paddingX:100,paddingY:100,
-        canvasTargetDom : ".js-canvasTargetDom",  
-        font : fontStyleList, color : "blue",   textString: textString, 
+        width: 300,
+        height: 300,
+        paddingX: 100,
+        paddingY: 100,
+        canvasTargetDom: ".js-canvasTargetDom",
+        font: fontStyleList,
+        color: "blue",
+        textString: textString,
         customFont: customFont
     };
     singatureText(paramSignatureText);
@@ -21,59 +29,71 @@ $('#nameForm').on('submit', function(e) {
 });
 
 
-function isDomObject(obj)
-{
+function isDomObject(obj) {
     return (obj.tagName ? "true" : "false");
 }
 
 
-function singatureText(options) {  
-  
-    options.font = (options.font)  || ["12px", "blue", "Arial"];    
+function singatureText(options) {
+
+    options.font = (options.font) || ["12px",   "Arial"];
     options.font = (options.font).join(" ");
-    options.fillStyle = (options.color)  || "black";  
+    options.fillStyle = (options.color) || "black";
     options.textString = options.textString || "Text-Signature !";
     options.paddingX = options.paddingX || 0;
     options.paddingY = options.paddingY || 0;
 
-    //inject custom font face from external url 
-    var styleFont = document.createTextNode("\
-    @font-face {\
-        font-family: '" + options.customFont.name + "';\
-        src: url('" + options.customFont.url + "')  ;\
-    }\
-    ") ; 
-    $('head').find(  "#text-signature" ).remove();
-    var styleElem =  $('<style></style>'); 
-    styleElem.attr('id', "text-signature");
-    styleElem.append(styleFont)
-    $('head').append( styleElem);
+    console.log(options);
+    //inject custom font face from external url   
+    $('head').find("#text-signature").remove();
+    var linkElem = $('<link rel="stylesheet">');
+    linkElem.attr('id', "text-signature");
+    linkElem.attr('href', options.customFont.url);
 
+    $('head').append(linkElem);
+
+
+ setTimeout(function(){ 
+
+
+    var uniquetime = new Date().getUTCMilliseconds();
 
     //generate canvas
-    var canvasSelectorDom =  $('<canvas></canvas>'); 
+    var canvasSelectorDom = $('<canvas></canvas>'); 
     canvasSelectorDom.attr('width', options.width);
-    canvasSelectorDom.attr('height', options.height);
+    canvasSelectorDom.attr('height', options.height);  
+    canvasSelectorDom.attr('id', "text-signature-" + uniquetime);
+    console.log(canvasSelectorDom )
+    // $('body').append( canvasSelectorDom);
+    // $('#' + "text-signature-" + uniquetime).remove();
+
+
+    var context = canvasSelectorDom[0].getContext("2d");
+    context.font = options.font;
+    context.fillStyle = options.fillStyle;
+    context.fillText(options.textString, options.paddingX, options.paddingY);
+
+    context.fillText(options.textString, options.paddingX, options.paddingY);
+
+    
  
-    var context =  canvasSelectorDom[0].getContext("2d");
-    context.font =options.font;
-    context.fillStyle = options.fillStyle;  
-    context.fillText(options.textString, options.paddingX, options.paddingY); 
-
-
-    // <canvas id="canvasId" width="500" height="500"></canvas> 
 
     var dataUrl = canvasSelectorDom[0].toDataURL();
-    var uniquetime = new Date().getUTCMilliseconds();
     var img = $('<img>'); //Equivalent: $(document.createElement('img'))
     img.attr('src', dataUrl);
     img.attr('text-signature-timestamp', uniquetime);
     img.attr('id', "text-signature-" + uniquetime);
 
-    if(options.canvasTargetDom ){
+
+
+    if (options.canvasTargetDom) {
         $(options.canvasTargetDom).html(img);
+    } else {
+        window.open(dataUrl, "text-signature image", "width=600, height=200");
     }
-    else{
-            window.open(dataUrl, "text-signature image", "width=600, height=200");
-    } 
+
+ // linkElem.load(function() { });
+ 
+
+  }, 3000);
 }
